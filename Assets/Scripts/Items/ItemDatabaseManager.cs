343d4865cbd30b6abb612c7975456792401ec70a
Eduardo Villar <alu.142886@usj.es>
2025-05-22 18:16:03 +0200

    item database manager finished

--- Assets/Scripts/Items/ItemDatabaseManager.cs
+++ Assets/Scripts/Items/ItemDatabaseManager.cs
@@ -1,11 +1,18 @@
 using UnityEngine;
 using System.Collections.Generic;
+using NUnit.Framework.Constraints;
+using static UnityEditor.Progress;
 
 public class ItemDatabaseManager : MonoBehaviour
 {
     public static ItemDatabaseManager Instance;
 
     public List<ItemBaseScript> m_AllItems = new List<ItemBaseScript>();
+    public List<ItemBaseScript> m_CommonItems = new List<ItemBaseScript>();
+    public List<ItemBaseScript> m_RareItems = new List<ItemBaseScript>();
+    public List<ItemBaseScript> m_EpicItems = new List<ItemBaseScript>();
+    public List<ItemBaseScript> m_LegendaryItems = new List<ItemBaseScript>();
+
 
     private void Awake()
     {
@@ -20,6 +27,7 @@ public class ItemDatabaseManager : MonoBehaviour
         DontDestroyOnLoad(gameObject);
 
         LoadAllItems();
+        CreateTierLists();
     }
 
     private void LoadAllItems()
@@ -36,9 +44,63 @@ public class ItemDatabaseManager : MonoBehaviour
         }
     }
 
+    private void CreateTierLists()
+    {
+        //Sorts by quality into lists
+        foreach (ItemBaseScript item in m_AllItems)
+        {
+            switch (item.m_QualityItem)
+            {
+                case ItemBaseScript.ItemQuality.COMMON:
+                    m_CommonItems.Add(item);
+                    break;
+                case ItemBaseScript.ItemQuality.RARE:
+                    m_RareItems.Add(item);
+                    break;
+                case ItemBaseScript.ItemQuality.EPIC:
+                    m_EpicItems.Add(item);
+                    break;
+                case ItemBaseScript.ItemQuality.LEGENDARY:
+                    m_LegendaryItems.Add(item);
+                    break;
+                default:
+                    Debug.LogWarning(item.name + " was not assigned proporly into a tier list");
+                    break;
+            }
+        }
+    }
+
     public ItemBaseScript GetRandomItem()
     {
         if (m_AllItems.Count == 0) return null;
-        return m_AllItems[Random.Range(0, m_AllItems.Count)];
+        
+        int randomItemIndex = Random.Range(0, m_AllItems.Count);
+        return m_AllItems[randomItemIndex];
+    }
+
+    public ItemBaseScript GetRandomItemOfQuality(ItemBaseScript.ItemQuality quality)
+    {
+        if (m_AllItems.Count == 0) return null;
+
+        int randomItemIndex = Random.Range(0, m_AllItems.Count);
+
+        switch (quality)
+        {
+            case ItemBaseScript.ItemQuality.COMMON:
+                return m_CommonItems[randomItemIndex];
+
+            case ItemBaseScript.ItemQuality.RARE:
+                return m_RareItems[randomItemIndex];
+
+            case ItemBaseScript.ItemQuality.EPIC:
+                return m_EpicItems[randomItemIndex];
+
+            case ItemBaseScript.ItemQuality.LEGENDARY:
+                return m_LegendaryItems[randomItemIndex];
+
+            default:
+                return null;
+        }
+
     }
 }
