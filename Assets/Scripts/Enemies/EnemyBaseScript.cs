4ebff9ff0eb99a0951b96308346804ab89c08f7f
Eduardo Villar <alu.142886@usj.es>
2025-05-22 11:00:46 +0200

    added components to enemy base script

--- Assets/Scripts/Enemies/EnemyBaseScript.cs
+++ Assets/Scripts/Enemies/EnemyBaseScript.cs
@@ -4,6 +4,9 @@ public class EnemyBaseScript : MonoBehaviour
 {
     [Header("BASE ENEMY PARAMS")]
     public HealthController m_HealthController = null;
+    public HealthController m_PlayerHealthController = null;
+    public Collider m_Collider = null;
+    public Animator m_Animator = null;
     public float m_ActionCooldown = 0.0f;
     public float m_TimeUntilNextAction = 0.0f;
     public bool m_IsMarked = false;
@@ -11,6 +14,9 @@ public class EnemyBaseScript : MonoBehaviour
     private void Start()
     {
         m_HealthController = GetComponent<HealthController>();
+        m_Collider = GetComponent<Collider>();
+        m_Animator = GetComponent<Animator>();
+
         m_TimeUntilNextAction = m_ActionCooldown;
     }
 
@@ -28,4 +34,6 @@ public class EnemyBaseScript : MonoBehaviour
     {
        
     }
+
+
 }
