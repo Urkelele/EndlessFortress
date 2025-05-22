35b8ea71bf7c228d308413d3846e838b7c103a94
Eduardo Villar <alu.142886@usj.es>
2025-05-22 10:36:20 +0200

    enemy base script

--- Assets/Scripts/Enemies/EnemyBaseScript.cs
+++ Assets/Scripts/Enemies/EnemyBaseScript.cs
@@ -3,6 +3,29 @@ using UnityEngine;
 public class EnemyBaseScript : MonoBehaviour
 {
     [Header("BASE ENEMY PARAMS")]
+    public HealthController m_HealthController = null;
+    public float m_ActionCooldown = 0.0f;
+    public float m_TimeUntilNextAction = 0.0f;
+    public bool m_IsMarked = false;
 
-    
+    private void Start()
+    {
+        m_HealthController = GetComponent<HealthController>();
+        m_TimeUntilNextAction = m_ActionCooldown;
+    }
+
+    private void Update()
+    {
+        if (m_TimeUntilNextAction < 0.0f)
+        {
+            //Reset timer
+            m_TimeUntilNextAction = m_ActionCooldown;
+            PerformAction();
+        }
+    }
+
+    public virtual void PerformAction()
+    {
+       
+    }
 }
