d28ef37e7b9a5fefc53b359c35519c602cf51ecf
Eduardo Villar <alu.142886@usj.es>
2025-05-22 21:02:04 +0200

    enemy spawning added

--- Assets/Scripts/CombatManager.cs
+++ Assets/Scripts/CombatManager.cs
@@ -1,10 +1,14 @@
 using NUnit.Framework;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class CombatManager : MonoBehaviour
 {
+    [Header("CONTROL VARS")]
+    public int m_NumOfRoomsBetweenBoss = 10;
+
     [Header("CLASSES")]
     private PlayerCombatScript m_PlayerCombatScript;
     public List<EnemyBaseScript> m_CombatEnemies;
@@ -12,7 +16,7 @@ public class CombatManager : MonoBehaviour
     public EnemyBaseScript m_CurrentEnemyTarget;
     
     [Header("POSITIONS")]
-    public Transform m_CharacterPosition;
+    public Transform m_PlayerPosition;
     [SerializeField] private Transform[] m_EnemyPositions = new Transform[3];
     [SerializeField] private List<GameObject> m_EnemyPool;
 
@@ -27,9 +31,77 @@ public class CombatManager : MonoBehaviour
     public float m_EpicDropChance = 0.15f;
     public float m_LegendaryDropChance = 0.05f;
 
+    [Header("ENEMY ROOM PREFABS")]
+    private List<GameObject> m_EnemyCompsList = new List<GameObject>();
+    [SerializeField] private string m_EnemyCompsFolderPath = "Prefabs/EnemyComps_Prefabs";
+    private List<GameObject> m_BossCompsList = new List<GameObject>();
+    [SerializeField] private string m_BossCompsFolderPath = "Prefabs/BossComps_Prefabs";
+    private GameObject m_CurrentComp = null;
+
+    private void Awake()
+    {
+        LoadCompsPrefabs(m_EnemyCompsList, m_EnemyCompsFolderPath);
+        LoadCompsPrefabs(m_BossCompsList, m_BossCompsFolderPath);
+    }
+
+
+    /// <summary>
+    /// Loads all gameobjects from a certain Resource/ path, deactivates them and puts them on a list
+    /// </summary>
+    /// <param name="list"></param>
+    /// <param name="prefabPath"></param>
+    private void LoadCompsPrefabs(List<GameObject> list, string prefabPath)
+    {
+        GameObject[] prefabList = Resources.LoadAll<GameObject>(prefabPath);
+
+        foreach (var prefab in prefabList)
+        {
+            list.Add(prefab);
+            prefab.SetActive(false);
+        }
+
+        Debug.Log($"Loaded {list.Count} prefabs into the queue.");
+    }
+
     private void StartBattle()
     {
+        MovePlayer();
+        SpawnEnemies();
         GetDefaultTargetEnemy();
+    }
+
+    private void MovePlayer()
+    {
+        m_PlayerCombatScript.transform.position = m_PlayerPosition.position;
+    }
+
+    /// <summary>
+    /// Instantiate a random comp
+    /// </summary>
+    private void SpawnEnemies()
+    {
+        m_CurrentComp = Instantiate(GetRandomComp());
+
+        for (int i = 0; i < m_CurrentComp.transform.childCount; i++)
+        {
+            m_CurrentComp.transform.GetChild(i).transform.position = m_EnemyPositions[i].position;
+        }
+    }
+
+    /// <summary>
+    /// Returns random comp prefab, depending on room count the comp will be a boss
+    /// </summary>
+    /// <returns></returns>
+    private GameObject GetRandomComp()
+    {
+        if(PlayerStats.instance.m_RoomsCleared % m_NumOfRoomsBetweenBoss == 0)
+        {
+            return m_EnemyCompsList[Random.Range(0, m_BossCompsList.Count)];
+        }
+        else
+        {
+            return m_EnemyCompsList[Random.Range(0, m_EnemyCompsList.Count)];
+        }
     }
 
     private void GetDefaultTargetEnemy()
@@ -39,11 +111,6 @@ public class CombatManager : MonoBehaviour
         int randEnemyPos = Random.Range(0, m_EnemyPositions.Length);
         m_PlayerCombatScript.m_TargetEnemy = m_CombatEnemies[randEnemyPos];
         m_CombatEnemies[randEnemyPos].m_ClickDetection.m_IsLastObjectClicked = true;
-    }
-
-    private void SpawnEnemies()
-    {
-
     }
 
     private void GiveRewards()
