4661d28bd7d347dc1f50b8b8f3c4dd4e23684aab
AlvaroGarSe <132345019+AlvaroGarSe@users.noreply.github.com>
2025-05-23 20:24:28 +0200

    invertory starting

---
+++ Assets/Scripts/UI/InventoryControllerUI.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class InventoryControllerUI : MonoBehaviour
+{
+    public GameObject m_SlotsPrefab;
+    
+
+    public void UpdateItemsInInventory()
+    {
+        
+    }
+}
