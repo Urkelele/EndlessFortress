2bf7ffab447ea657f73a36b9147f2473a2b951d9
Urko Aguillo <118058363+Urkelele@users.noreply.github.com>
2025-05-23 12:15:04 +0200

    Cambios enemigos

---
+++ Assets/Scripts/Enemies/TreantScript.cs
@@ -0,0 +1,47 @@
+using System;
+using UnityEngine;
+
+public class TreantScript : EnemyBaseScript
+{
+    [SerializeField] private float m_HealAmount = 10.0f;
+    private EnemyBaseScript m_LowestHealthEnemy = null;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        base.Update();   
+    }
+
+    public override void PerformAction()
+    {
+        GetLowestHealthEnemy();
+        if(m_LowestHealthEnemy != null)
+        {
+            m_LowestHealthEnemy.m_HealthController.HealDamage(m_HealAmount);
+        }
+    }
+
+    private void GetLowestHealthEnemy()
+    {
+        foreach (EnemyBaseScript enemy in m_CombatManager.m_CombatEnemies)
+        {
+            if (m_LowestHealthEnemy == null && !enemy.GetComponent<TreantScript>())
+            {
+                m_LowestHealthEnemy = enemy;
+            }
+            else
+            {
+                if (m_LowestHealthEnemy.m_HealthController.m_HealthPoints > enemy.m_HealthController.m_HealthPoints)
+                {
+                    m_LowestHealthEnemy = enemy;
+                }
+            }
+
+        }
+    }
+}
