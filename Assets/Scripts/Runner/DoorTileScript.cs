4d27c6beb7e6d19a14397cf8321d2993de4e2443
Eduardo Villar <alu.142886@usj.es>
2025-05-24 12:06:13 +0200

    doorTile Script finished

--- Assets/Scripts/Runner/DoorTileScript.cs
+++ Assets/Scripts/Runner/DoorTileScript.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 public class DoorTileScript : MonoBehaviour
 {
     public List<GameObject> m_NonBossDoorPrefabs = new List<GameObject>();
+    private List<GameObject> m_DoorList = new List<GameObject>(); //List that is used to spawn the doors
     public GameObject m_BossDoorPrefab = null;
-    public List<GameObject> m_SpawnedDoors = new List<GameObject>();
+    private List<GameObject> m_SpawnedDoors = new List<GameObject>();
     public List<Transform> m_DoorPositions = new List<Transform>();
 
     private void OnEnable()
@@ -14,21 +15,32 @@ public class DoorTileScript : MonoBehaviour
         if(RoomTransitionManager.instance.m_NextRoomIsBoss)
         {
             //Instantiate the bossDoor as a child of the middle door position
-            Instantiate(m_BossDoorPrefab, m_DoorPositions[1]);
-            m_SpawnedDoors.Add(m_BossDoorPrefab);
+            GameObject bossDoor = Instantiate(m_BossDoorPrefab, m_DoorPositions[1]);
+            m_SpawnedDoors.Add(bossDoor);
         }
         else
         {
+            //Get random doors without repetition and instantiate them in the correct positions
+            m_DoorList = new List<GameObject>(m_NonBossDoorPrefabs);
             for(int i = 0; i < m_DoorPositions.Count; i++)
             {
-                
+                GameObject randDoor = Instantiate(PopRandomDoorFromList(), m_DoorPositions[i]);
+                m_SpawnedDoors.Add(randDoor);
             }
         }
     }
 
-    public void PopRandomDoorFromList()
+    /// <summary>
+    /// Returns a random door and remove it from the list
+    /// </summary>
+    /// <returns></returns>
+    public GameObject PopRandomDoorFromList()
     {
-
+        Debug.Log(m_DoorList.Count);
+        int randomIndex = Random.Range(0, m_DoorList.Count);
+        GameObject randDoor = m_DoorList[randomIndex];
+        m_DoorList.Remove(randDoor);
+        return randDoor;
     }
 
     //When the tile is disabled, destroy every spawned door
@@ -38,6 +50,7 @@ public class DoorTileScript : MonoBehaviour
         {
             Destroy(go);
         }
+
     }
 
 }
