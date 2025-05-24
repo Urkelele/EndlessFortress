cc7cac6f4b2afb5f78e9996a9f091710c9d83dd9
Urko Aguillo <118058363+Urkelele@users.noreply.github.com>
2025-05-24 11:07:44 +0200

    si

--- Assets/Scripts/Runner/PasiveItemPickUpScript.cs
+++ Assets/Scripts/Runner/PasiveItemPickUpScript.cs
@@ -4,12 +4,14 @@ public class PasiveItemPickUpScript : MonoBehaviour
 {
     private Sprite m_ItemSprite = null;
     private ItemBaseScript m_ItemPrefab = null;
+    private Collider m_Collider = null;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         int index = Random.Range(0, ItemDatabaseManager.Instance.m_UnlockedItems.Count);
         m_ItemPrefab = ItemDatabaseManager.Instance.m_UnlockedItems[index];
         m_ItemSprite = m_ItemPrefab.m_SpriteItem;
+        m_Collider = GetComponent<Collider>();
     }
 
     // Update is called once per frame
